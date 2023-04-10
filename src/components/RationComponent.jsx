ceedb8369b98d8ef9d326ffc808321bf13f50ecc
romashaa <mariia.romanenko@ukma.edu.ua>
2023-04-10 18:28:28 +0300

    before adding redux

--- src/components/RationComponent.jsx
+++ src/components/RationComponent.jsx
@@ -10,59 +10,72 @@ import supper from '../img/supper.jpg'
 import {Col, Container, Form, ListGroup, Modal, Row} from "react-bootstrap";
 import {FaPlus} from "react-icons/fa";
 import TimePicker from "react-time-picker";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
 import InputGroup from 'react-bootstrap/InputGroup';
 import axios from "axios";
-import Select from 'react-select';
 
 const RationComponent = () => {
     const [open, setOpen] = React.useState(true);
     const [show, setShow] = useState(false);
     const [value, onChange] = useState('10:00');
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchDishes, setSearchDishes] = useState([]);
+    const [grams, setGrams] = useState('')
+    const [dishOptions, setDishOptions] = useState([])
+    const [dishName, setDishName] = useState("")
+    const [currentDish, setCurrentDish] = useState({})
+    const [suggestions, setSuggestions] = useState([])
 
+    const handleGramsChange = (event) => {
+        const inputValue = event.target.value.slice(0, 4); // limit input to 4 characters
+        setGrams(inputValue);
+    }
 
+    const addDish = () => {
+
+    }
     useEffect(() => {
-        axios.get(`/dishes/search?term=`).then(response => {
-            setSearchDishes(response.data);
-        });
-    }, []);
+        const loadDishes = async () => {
+            const response = await axios.get("/dishes/all");
+            setDishOptions(response.data)
+        }
+        loadDishes();
+    }, [])
 
-    // const handleSearchInputChange = async (event) => {
-    //     const term = event.target.value;
-    //     setSearchTerm(term);
-    //     if (term.length >= 1) {
-    //         const response = await axios.get(`/dishes/search?term=`);
-    //         const searchDishes = response.data;
-    //         setSearchDishes(searchDishes);
-    //     }
-    //     console.log("1")
-    //     console.log(searchDishes)
-    // };
-
-    function print(){
-        console.log(searchDishes)
+    const onSuggestHandler = (text) => {
+        setDishName(text);
+        setSuggestions([]);
+        setCurrentDish(dishOptions.find(dish=> dish.dishName===text))
     }
 
-    const handleSearchTermClick = (term) => {
-        setSearchTerm(term);
-        // Perform the search using the selected search term
-    };
-    const handleInput = (event) => {
-        setSearchTerm(event.target.value);
-    };
-    function addBreakfast(){
+    const handleDishInputChange = (dishName) => {
+        let matches = []
+        if (dishName.length > 0) {
+            const dishnames = dishOptions.map(dish => dish.dishName)
+
+            matches = dishnames.filter(dishname => {
+                const regex = new RegExp(`${dishName}`, "gi");
+                return dishname.match(regex)
+            })
+        }
+        console.log(matches)
+        setSuggestions(matches)
+        setDishName(dishName)
+    }
+
+
+    function addBreakfast() {
 
     }
-    function addSnack(){
+
+    function addSnack() {
 
     }
-    function addDinner(){
+
+    function addDinner() {
 
     }
-    function addSupper(){
+
+    function addSupper() {
 
     }
 
@@ -74,7 +87,7 @@ const RationComponent = () => {
             <div className="col-md-5 col-sm-8 m-auto menuList">
                 <ListGroup as="ul">
                     <ListGroup.Item as="li" variant="flush">
-                        Сніданок<FaPlus className="menuItemBtn" onClick={handleShow} />
+                        Сніданок<FaPlus className="menuItemBtn" onClick={handleShow}/>
                         <div>
                             <ListGroup as="ul" variant="flush" className="menuListItem">
                                 <ListGroup.Item as="li">
@@ -88,13 +101,14 @@ const RationComponent = () => {
                             </ListGroup>
                         </div>
                     </ListGroup.Item>
-                    <ListGroup.Item as="li" variant="flush">Обід <FaPlus className="menuItemBtn" onClick={addDinner}/></ListGroup.Item>
+                    <ListGroup.Item as="li" variant="flush">Обід <FaPlus className="menuItemBtn"
+                                                                         onClick={addDinner}/></ListGroup.Item>
                     <ListGroup.Item as="li" variant="flush">Вечеря <FaPlus className="menuItemBtn" onClick={addSupper}/></ListGroup.Item>
                     <ListGroup.Item as="li" variant="flush">Перекус <FaPlus className="menuItemBtn" onClick={addSnack}/></ListGroup.Item>
                 </ListGroup>
             </div>
 
-            <Modal show={show} onHide={handleClose} style={{backgroundColor: "rgba(0, 0, 0, 0)", height:'100%'}}>
+            <Modal show={show} onHide={handleClose} style={{backgroundColor: "rgba(0, 0, 0, 0)", height: '100%'}}>
                 <Modal.Header closeButton>
                     <Modal.Title>Прийом їжі</Modal.Title>
                 </Modal.Header>
@@ -102,49 +116,70 @@ const RationComponent = () => {
                     <Form>
                         <InputGroup className="mb-3">
                             <InputGroup.Text id="basic-addon1">
-                                <FontAwesomeIcon icon={faSearch} />
+                                <FontAwesomeIcon icon={faSearch}/>
                             </InputGroup.Text>
-                            {/*{searchDishes.length > 0 && (*/}
-                                <Select
-                                    placeholder="Введіть текст для пошуку страви"
-                                    value={searchTerm}
-                                    onChange={print}
-                                    options={searchDishes}
-                                    isSearchable
-                                >
-                                    {/*{searchDishes.map((term) => (*/}
-                                    {/*    <option key={term} onClick={() => handleSearchTermClick(term)}>*/}
-                                    {/*        {term}*/}
-                                    {/*    </option>*/}
-                                    {/*))}*/}
-                                </Select>
-                            {/*)}*/}
+                            <Form.Control
+                                placeholder="Введіть текст для пошуку страви"
+                                type="text"
+                                value={dishName}
+                                onChange={e => handleDishInputChange(e.target.value)}
+                                className="dishInput"
+                                style={{display: "inline-block", width: "85%"}}
+                            />
+                            <div>
+                                {suggestions && suggestions.map((suggestion, i) =>
+                                    <div
+                                        className="dishSuggestion"
+                                        style={{display: "block", marginLeft: "42px", width: "85%"}}
+                                        key={i}
+                                        onClick={() => onSuggestHandler(suggestion)}
+                                    >
+                                        {suggestion}
+                                    </div>
+                                )}
+                            </div>
+                            <Form.Text className="text-muted">
+                               На 100 г: (Ккал: {currentDish.calories}, Б:{currentDish.bilki}, Ж:{currentDish.fats}, В:{currentDish.vuhlevody} )
+                            </Form.Text>
                         </InputGroup>
                         <Form.Group className="mb-3 dateAndMealRow">
-                            <TimePicker className="dateAndMeal" onChange={onChange} value={value} />
-                            <Form.Select  className="dateAndMeal">
+                            <TimePicker className="dateAndMeal" onChange={onChange} value={value}/>
+                            <Form.Select className="dateAndMeal">
                                 <option value="1">Сніданок</option>
                                 <option value="2">Обід</option>
                                 <option value="3">Вечеря</option>
                                 <option value="3">Перекус</option>
                             </Form.Select>
                         </Form.Group>
-                        <Form.Group className="mb-3">
+                        <InputGroup className="mb-3">
                             <Form.Label>Маса:</Form.Label>
-                            <Form.Control placeholder="Введіть значення"/>
-                        </Form.Group>
+                            <Form.Control
+                                type="number"
+                                placeholder="Введіть значення"
+                                inputMode="numeric"
+                                onChange={handleGramsChange}
+                                value={grams}
+                                min={0}
+                                max={2000}
+                                maxLength={4}
+                                aria-describedby="gramsDescr"
+                            />
+                            <InputGroup.Text id="gramsDescr">г</InputGroup.Text>
 
-                        {/*<Form.Group>*/}
+                        </InputGroup>
+                        <Form.Text className="text-muted">
+                            На {grams} г: (Ккал: {currentDish.calories/100*grams}, Б:{currentDish.bilki/100*grams}, Ж:{currentDish.fats/100*grams}, В:{currentDish.vuhlevody/100*grams} )
+                        </Form.Text>
 
-                        {/*</Form.Group>*/}
+
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" >
+                    <Button variant="secondary">
                         Close
                     </Button>
                     <Button variant="primary" >
-                        Save Changes
+                         Додати
                     </Button>
                 </Modal.Footer>
             </Modal>
